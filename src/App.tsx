276de31a51249367fee2146735d72385cd2445f1
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2026-06-19 12:03:58 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center my-10 underline">
+        Welcome to React
+      </h1>
+    </>
+  );
+}
+
+export default App;
